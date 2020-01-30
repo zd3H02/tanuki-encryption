1cc31624b6ae55b08e340693fb755b4452ee7df1
zd3H02 <zd3h02@gmail.com>
2020-01-31 03:26:28 +0900

    add class LexicalAnalyzer

--- assets/index.php
+++ assets/index.php
@@ -11,63 +11,133 @@ const ENCRYPTION_STRENGTH = [
     50,//%
 ];
 
-
-class LexicalAnalysisTanuki
+class LexicalAnalyzer
 {
-    protected $pos_i;
-    protected $max_text_i;
-    protected $text;
-    protected $tanuki_tokens;
-    protected $encryption_lex;
-    protected $encryption_lex_word_count
-    __construct($text, $encryption_lex) {
-        $this->pos_i = 0;
-        $this->max_text_i = mb_strlen($text) - 1;
-        $this->text = $text;
-        $this->tanuki_tokens = [];
-        $this->encryption_lex = $encryption_lex;
-        $this->encryption_lex_word_count = mb_strlen($encryption_lex) - 1;
-        $this->lexicalAnalysisExecute();
-    }
-    function lexicalAnalysisExecute() {
+    const TOKEN_TYPE = "token_type";
+    const TOKEN = "token";
+    const TOKEN_TYPE_ENCRYPTION = "encryption";
+    const TOKEN_TYPE_NORMAL = "normal";
+    const TOKEN_TYPE_NORMAL_NEXT_ENCRYPTION = "next_encryption";
+    const TOKEN_TYPE_NORMAL_PREV_ENCRYPTION = "prev_encryption";
+    const TOKEN_TYPE_NORMAL_WHILE_ENCRYPTION = "while_encryption";
+    const TOKEN_TYPE_NONE = "none";
+    function __construct($string_to_lecical_analyze, $reserved_word_encryption, $reserved_word_encryption_appear_rato) {
+        $this->now_char_pos_i = 0;
+        $this->max_char_pos_i = mb_strlen($string_to_lecical_analyze) - 1;
+        $this->string_to_lecical_analyze = $string_to_lecical_analyze;
+        $this->temp_analyzed_tokens = [];
+        $this->analyzed_tokens = [];
+        $this->reserved_word_encryption = $reserved_word_encryption;
+        $this->count_of_reserved_word_encryption = mb_strlen($reserved_word_encryption);
+        $this->reserved_word_encryption_appear_rato = $reserved_word_encryption_appear_rato;
+        $this->lexicalAnalyzerExecute();
+        $this->lexicalAnalyzerBreakDownExecute();
 
-        $this->getChar();
+        //echo "<p>--string_to_lecical_analyze-------------</p>";
+        var_dump($this->now_char_pos_i);
+        var_dump($this->max_char_pos_i);
+        var_dump($this->string_to_lecical_analyze);
+        var_dump($this->temp_analyzed_tokens);
+        var_dump($this->analyzed_tokens);
+        var_dump($this->reserved_word_encryption);
+        var_dump($this->count_of_reserved_word_encryption);
     }
-    function getChar() {
-        $is_out_of_range = $pos_i >= max_text_i
+    function getChar($char_pos_i) {
+        $is_out_of_range = $this->now_char_pos_i > $this->max_char_pos_i;
         if ($is_out_of_range) {
             return "";
         } else {
-            return  mb_substr($input_text, $this->pos_i, 1);
+            $get_char = mb_substr($this->string_to_lecical_analyze, $char_pos_i, 1);
+            return $get_char;
+        }
+    }
+    function seekEncryptionToken() {
+        $get_chars = "";
+        for ($get_char_i = 0; $get_char_i < $this->count_of_reserved_word_encryption; $get_char_i++) {
+            $next_get_char_pos_i = $get_char_i + $this->now_char_pos_i;
+            $get_chars = $get_chars . $this->getChar($next_get_char_pos_i);
+        }
+        $is_reserved_word_encryption = $get_chars === $this->reserved_word_encryption;
+        if ($is_reserved_word_encryption) {
+            $this->now_char_pos_i += $this->count_of_reserved_word_encryption;
+            return $get_chars . $this->seekEncryptionToken();
+        } else {
+            return "";
+        }
+    }
+    function tokenEncryption() {
+        $get_chars = $this->seekEncryptionToken();
+        $is_reserved_word_encryption_exists = $get_chars !== "";
+        if ($is_reserved_word_encryption_exists) {
+            $this->temp_analyzed_tokens[] = [
+                $this::TOKEN_TYPE   => $this::TOKEN_TYPE_ENCRYPTION,
+                $this::TOKEN        => $get_chars,
+            ];
+        }
+    }
+    function tokenNormal() {
+        $this->temp_analyzed_tokens[] = [
+            $this::TOKEN_TYPE   => $this::TOKEN_TYPE_NORMAL,
+            $this::TOKEN        => $this->getChar($this->now_char_pos_i),
+        ];
+        $this->now_char_pos_i++;
+    }
+    function tokenTypeBreakDown($token_i) {
+        $token_type = $this->temp_analyzed_tokens[$token_i][$this::TOKEN_TYPE];
+        echo $token_type . "----";
+        $is_token_type_normal = $token_type === $this::TOKEN_TYPE_NORMAL;
+        $is_tokens = count($this->temp_analyzed_tokens) > 1;
+        $is_normal_break_down_execute = $is_token_type_normal && $is_tokens;
+        if ($is_normal_break_down_execute) {
+            $is_first_token = $token_i === 0;
+            $is_last_token = $token_i === count($this->temp_analyzed_tokens) - 1;
+            if ($is_first_token) {
+                $next_token_i = $token_i + 1;
+                $prev_token_type = $this::TOKEN_TYPE_NONE;
+                $next_token_type = $this->temp_analyzed_tokens[$next_token_i][$this::TOKEN_TYPE];
+            } elseif ($is_last_token) {
+                $prev_token_i = $token_i - 1;
+                $prev_token_type = $this->temp_analyzed_tokens[$prev_token_i][$this::TOKEN_TYPE];
+                $next_token_type = $this::TOKEN_TYPE_NONE;
+            } else {
+                $prev_token_i = $token_i - 1;
+                $next_token_i = $token_i + 1;
+                $prev_token_type = $this->temp_analyzed_tokens[$prev_token_i][$this::TOKEN_TYPE];
+                $next_token_type = $this->temp_analyzed_tokens[$next_token_i][$this::TOKEN_TYPE];
+            }
+            $is_token_type_prev     = $prev_token_type === $this::TOKEN_TYPE_ENCRYPTION;
+            $is_token_type_next     = $next_token_type === $this::TOKEN_TYPE_ENCRYPTION;
+            $is_token_type_while    = $is_token_type_prev && $is_token_type_next;
+            if ($is_token_type_while ) {
+                return $this::TOKEN_TYPE_NORMAL_WHILE_ENCRYPTION;
+            } elseif ($is_token_type_prev) {
+                return $this::TOKEN_TYPE_NORMAL_PREV_ENCRYPTION;
+            } elseif ($is_token_type_next) {
+                return $this::TOKEN_TYPE_NORMAL_NEXT_ENCRYPTION;
+            } else {
+                return $this::TOKEN_TYPE_NORMAL;
+            }
+        } else {
+            return $token_type;
+        }
+    }
+    function lexicalAnalyzerBreakDownExecute() {
+        for ($token_i = 0; $token_i < count($this->temp_analyzed_tokens); $token_i++) {
+            $this->analyzed_tokens[] = [
+                $this::TOKEN_TYPE   => $this->tokenTypeBreakDown($token_i),
+                $this::TOKEN        => $this->temp_analyzed_tokens[$token_i][$this::TOKEN],
+            ];
+        }
+    }
+    function lexicalAnalyzerExecute() {
+        $this->tokenEncryption();
+        $this->tokenNormal();
+        if ($this->now_char_pos_i <= $this->max_char_pos_i) {
+            $this->lexicalAnalyzerExecute();
         }
     }
 }
 
-
-function  lexicalAnalysisTanuki ($input_text, $encryption_string) {
-    $tanuli_tokens = [];
-    $input_text_word_count = mb_strlen($input_text);
-    for ($i=0; $i < $input_text_word_count; $i++) {
-    }
-
-    retun $tanuli_tokens;
-}
-
-function getChar($pos, $string) {
-    $input_text_word_count = mb_strlen($input_text);
-
-    if ()
-    $char =
-
-    return
-}
-
-function encryptionString ($pos, $string) {
-    $char = mb_substr($input_text, $i, 1);
-    $next_char = $char = mb_substr($input_text, $i + 1, 1);
-    $char =
-    if ()
-}
 
 
 require_once "util.inc.php";
@@ -80,12 +150,10 @@ $encryption_strength    = $_POST["encryption_strength"];
 
 if ($is_encryption) {
     $input_text_word_count = mb_strlen($input_text);
-    $encryption_string = ENCRYPTION_STRING[$encryption_type];
-    $encryption_string_appear_rato = ENCRYPTION_STRENGTH[$encryption_strength];
-    for ($i=0; $i < $input_text_word_count; $i++) {
-        //echo "<p>" . mb_substr($input_text, $i, 1) . "</p>";
+    $reserved_word_encryption = ENCRYPTION_TYPE[$encryption_type];
+    $reserved_word_encryption_appear_rato = ENCRYPTION_STRENGTH[$encryption_strength];
+    $test = new LexicalAnalyzer($input_text, $reserved_word_encryption, $reserved_word_encryption_appear_rato);
 
-    }
 } elseif (is_composite) {
 
 }
